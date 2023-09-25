1f4d9720eccf54eb936783fa9a0b5ba3db22f9a2
veera542 <132325580+veera542@users.noreply.github.com>
2023-09-25 16:27:26 -0700

    Create update_user.php

---
+++ SingletonPatternLogin/update_user.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once('UserController.php');
+
+session_start();
+
+$userModel = new UserModel($db);
+$userController = new UserController($userModel);
+
+if (!$userController->isLoggedIn()) {
+    header("Location: login.php");
+    exit();
+}
+
+if (isset($_POST['update'])) {
+    $newUsername = $_POST['newUsername'];
+    $newPassword = $_POST['newPassword'];
+
+    if ($userController->updateUser($_SESSION['user_id'], $newUsername, $newPassword)) {
+        // User update successful, you can redirect to a success page
+        header("Location: welcome.php");
+        exit();
+    } else {
+        $updateError = "Update failed. Please try again.";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Update User</title>
+</head>
+<body>
+    <h2>Update User</h2>
+    <form method="post" action="update_user.php">
+        <label for="newUsername">New Username:</label>
+        <input type="text" name="newUsername" required><br>
+        <label for="newPassword">New Password:</label>
+        <input type="password" name="newPassword" required><br>
+        <input type="submit" name="update" value="Update">
+        <?php if (isset($updateError)): ?>
+            <p><?php echo $updateError; ?></p>
+        <?php endif; ?>
+    </form>
+</body>
+</html>
